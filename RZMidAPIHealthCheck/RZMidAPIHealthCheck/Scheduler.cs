f2300d22f4f10728a5f6eb338928bd3e8000834f
Shital Suryawanshi <shital.suryawanshi@rigzone.com>
2020-01-29 20:25:42 +0530

    Mid API health check source code

---
+++ RZMidAPIHealthCheck/RZMidAPIHealthCheck/Scheduler.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Net.Http;
+using System.ServiceProcess;
+using System.Timers;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+using System.Json;
+using System.Data.SqlClient;
+
+
+namespace RZMidAPIHealthCheck
+{
+    public partial class Scheduler : ServiceBase
+    {
+        //string SystemURL = ConfigurationManager.AppSettings["SystemURL"].ToString();
+        private Timer timer = null;
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Set up a timer that triggers every minute.
+            timer = new Timer();
+            this.timer.Interval = 60000; // 60 seconds
+            this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Tick);
+            timer.Enabled = true;
+            Library.WriteErrorLog("The job was run on " + DateTime.Now);
+        }
+
+        public void timer_Tick(object sender, ElapsedEventArgs args)
+        {
+            string strEmail = "";
+            string stillaliveOne = ConfigurationManager.AppSettings["stillaliveone"].ToString();
+            string stillaliveTwo = ConfigurationManager.AppSettings["stillalivetwo"].ToString();
+            string conString = ConfigurationManager.AppSettings["PortalEmailerConnectionString"].ToString();
+
+            // Create an HttpClient instance
+            HttpClient clientOne = new HttpClient();
+            clientOne.BaseAddress = new Uri(stillaliveOne);
+
+            strEmail = "------------------------------------------------------------ \n";
+
+            strEmail = strEmail + "MidAPI STATUS \n";
+
+            strEmail = strEmail + "------------------------------------------------------------\n\n";
+
+            strEmail = strEmail + "JOB RUN:  " + DateTime.Now + "\n";
+
+            strEmail = strEmail + "STATUS:   Failed \n";
+            //Call stillaliveOne endpoint
+            try
+            {
+                HttpResponseMessage response = clientOne.GetAsync("heartbeat/v1/stillalive").Result;
+                var res = response.Content.ReadAsStringAsync();
+                var root = JsonValue.Parse(res.Result);
+                string status = root["StillAlive"].ToString();
+                if (status == "yes")
+                {
+                    Library.WriteErrorLog("The middleware server " + stillaliveOne + "heartbeat/v1/stillalive is live. Server status is " + status);
+                }
+                else
+                {
+                    strEmail = strEmail + "MESSAGES: The middleware server " + stillaliveOne + "heartbeat/v1/stillalive is down. \n";
+                    Library.WriteErrorLog("The middleware server " + stillaliveOne + "heartbeat/v1/stillalive is down. Server status is " + status);
+                }
+
+                //Call stillaliveTwo endpoint
+                HttpClient clientTwo = new HttpClient();
+                clientTwo.BaseAddress = new Uri(stillaliveTwo);
+                HttpResponseMessage resp = clientTwo.GetAsync("heartbeat/v1/stillalive").Result;
+                var read = resp.Content.ReadAsStringAsync();
+                var parse = JsonValue.Parse(read.Result);
+                string statusTwo = parse["StillAlive"].ToString();
+                if (statusTwo == "yes")
+                {
+                    Library.WriteErrorLog("The middleware server " + stillaliveTwo + "heartbeat/v1/stillalive is live. Server status is " + status);
+                }
+                else
+                {
+                    strEmail = strEmail + "MESSAGES: The middleware server " + stillaliveTwo + "heartbeat/v1/stillalive is down. \n";
+                    Library.WriteErrorLog("The middleware server " + stillaliveTwo + "heartbeat/v1/stillalive is down. Server status is " + status);
+                }
+
+                if ((status != "yes") || (statusTwo != "yes"))
+                {
+                    //SCHEDULE EMAIL
+                    SqlConnection conn = new SqlConnection(conString);
+                    conn.Open();
+
+                    //Call stored procedure to schedule email
+                    SqlCommand cmd = new SqlCommand("spu_MidAPI_Health_Check", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@Email_Body", SqlDbType.VarChar).Value = strEmail;
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                strEmail = strEmail + "MESSAGES: The job failed. Exception occured. " + ex + " \n";
+                SqlConnection conn = new SqlConnection(conString);
+                conn.Open();
+
+                //Call stored procedure to schedule email
+                SqlCommand cmd = new SqlCommand("spu_MidAPI_Health_Check", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Email_Body", SqlDbType.VarChar).Value = strEmail;
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                Library.WriteErrorLog("The job failed. Exception occured. " + ex);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            timer.Enabled = false;
+            Library.WriteErrorLog("RZWindows service stopped");
+        }
+    }
+}
